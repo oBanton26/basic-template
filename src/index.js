2cdf124681d9bb5230dd5a696637933e16855755
Ulysse <ulysse.aubanton@gmail.com>
2025-07-02 11:12:56 +0200

    Reset files to create template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import "./styles.css";
-import { createTodo , createList} from "./applogic";
-import { display } from "./DOMlogic.js";
-import { setUpButtons } from "./buttonsLogic.js";
-import { populateStorage, retrieveStorage } from "./localStorageLogic.js";
-
-
-
-retrieveStorage();
-setUpButtons();
-display();
